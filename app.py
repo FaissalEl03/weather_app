a4baa51c01e54483dc639a162b97bda5c0bddb0a
FaissalEl03 <elouidaniF@gmail.com>
2025-04-27 23:24:24 +0100

    added app

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def weather():
     return render_template('weather.html', weather=weather_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))  # Railway will set PORT
